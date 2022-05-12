0dfc604381ee959e9b67596a3ee9d8d1436e31ff
Joseph Kleinkopf <joseph.kleinkopf@gmail.com>
2022-05-12 09:47:46 -0600

    modified projects page

--- myJS/scripts.js
+++ myJS/scripts.js
@@ -1,3 +1,17 @@
+window.onload = function() {
+
 $(function () {
   $("#navbar").load("nav.html");
 });
+
+$(".mapping-project").click(function() {
+  window.open($(this).find("a").attr("href")); 
+  return false;
+});
+
+$(".project").click(function() {
+  window.location = $(this).find("a").attr("href"); 
+  return false;
+});
+
+}
