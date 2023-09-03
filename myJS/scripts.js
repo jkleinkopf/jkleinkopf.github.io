4bced0fb4601b04d305c3806a3dfec07a4de1e0a
Joseph Kleinkopf <joseph.kleinkopf@gmail.com>
2023-09-03 09:00:11 -0600

    fixed text and updated dates

--- myJS/scripts.js
+++ myJS/scripts.js
@@ -1,24 +1,24 @@
-window.onload = function() {
+window.onload = function () {
 
-$(function () {
-  $("#navbar").load("nav.html");
-});
-
-$(".mapping-project").click(function() {
-  window.open($(this).find("a").attr("href")); 
-  return false;
-});
-
-$(".project").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
-});
-
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()/2);
+  $(function () {
+    $("#navbar").load("nav.html");
   });
-});
+
+  $(".mapping-project").click(function () {
+    window.open($(this).find("a").attr("href"));
+    return false;
+  });
+
+  $(".project").click(function () {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height() / 2);
+    });
+  });
 
 }
